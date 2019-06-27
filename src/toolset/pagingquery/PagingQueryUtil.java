776ab61132669c98c7e5c00de6119fc710055f73
markMe1024 <com.mark.yu2015@gmail.com>
2019-06-27 14:28:04 +0800

    更新代码

--- src/toolset/pagingquery/PagingQueryUtil.java
+++ src/toolset/pagingquery/PagingQueryUtil.java
@@ -2,32 +2,43 @@ package toolset.pagingquery;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class PagingQueryUtil {
 	
 	/** 
-	 * @Description 从完整列表中截取出符合分页条件的列表
+	 * @Description 从全部数据中截取出符合分页条件的数据
 	 * @Author .Mark
 	 * @Date   2018年3月27日
 	 */
-	public static List<Map<String, Object>> subList(List<Map<String, Object>> entireList, int pageNo, int pageSize) {
-		// 限制入参
-		if(entireList == null || entireList.isEmpty()) { return new ArrayList<>(); }
-		if(pageNo <= 0 || pageSize <= 0) { return new ArrayList<>(); }
-		// 完整列表容量
+	public static List<Object> subList(List<Object> entireList, int currentPage, int pageSize) {
+		// 1.如果全部数据为空，返回空
+		if (entireList == null || entireList.isEmpty()) { 
+			return new ArrayList<>(); 
+		}
+
+		// 2.计算截取的起始、终止位置，计算总数据量
+		int startIndex = (currentPage - 1) * pageSize;
+		int endIndex = currentPage * pageSize - 1;
 		int totals = entireList.size();
-		// 如果请求数据起始范围超过了总记录数，返回空列表
-		if((pageNo-1)*pageSize+1 > totals) {
+		
+		// 3.如果startIndex小于0，返回全部数据
+		if (startIndex < 0) { 
+			return entireList; 
+		}
+		
+		// 4.如果startIndex超出总数据范围，返回空列表
+		if (startIndex > totals - 1) {
 			return new ArrayList<>();
 		}
-		// 如果请求数据范围的起始范围小于等于总记录数，而终止范围大于等于总记录数，则截取到entireList末尾
-		else if((pageNo-1)*pageSize + 1 <= totals && totals <= pageNo*pageSize) {
-			return entireList.subList((pageNo-1)*pageSize, totals);
+		
+		// 5.如果endIndex超出总数据范围，则截取到末尾
+		if (endIndex > totals - 1) {
+			return entireList.subList(startIndex, totals);
 		}
-		// 请求数据起始终止范围都在总记录范围内，正常截取
+		
+		// 6.如果endIndex没有超出总数据范围，则截取到endIndex
 		else {
-			return entireList.subList((pageNo-1)*pageSize, pageNo*pageSize);
+			return entireList.subList(startIndex, endIndex + 1);
 		}
 	}
 }
